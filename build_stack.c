94aa14f0b9bf1e5fe5ba9d813665b42c985275f8
mesalmann <mervesalman_07@hotmail.com>
2025-09-24 19:02:13 +0300

    quick info for build_stack

--- build_stack.c
+++ build_stack.c
@@ -46,13 +46,13 @@ static int  has_dup(t_list *a, int val)
 
 static void sign_handle(const char *str, int *start, int *len, long *sign, t_list **a)
 {
-    if (*len == 0)
+    if (*len == 0) //sayı yoksa error
         exit_with_error(a);
-    if (str[*start] == '-' || str[*start] == '+')
+    if (str[*start] == '-' || str[*start] == '+') 
     {
-        if (*len == 1)
+        if (*len == 1) //sadece "+" ya da "-" gibi durumlar error
             exit_with_error(a);
-        if (str[*start] == '-')
+        if (str[*start] == '-') 
             *sign = -1;
         *start = *start + 1;
         *len = *len - 1;
@@ -68,17 +68,17 @@ static int  safe_atoi_long(const char *str, int start, int len, t_list **a)
     i = 0;
     res = 0;
     sign = 1;
-    sign_handle(str, &start, &len, &sign, a);
+    sign_handle(str, &start, &len, &sign, a); // - li durumları handle ediyoruz 
     while (i < len)
     {
-        if (str[start + i] < '0' || str[start + i] > '9')
+        if (str[start + i] < '0' || str[start + i] > '9')//is valid number kontrolü yapıyoruz
             exit_with_error(a);
-        res = res * 10 + sign * (str[start + i] - '0');
-        if (res > INT_MAX || res < INT_MIN)
+        res = res * 10 + sign * (str[start + i] - '0');//int min max kontrolü yapabilmek için longa çeviriyoruz 
+        if (res > INT_MAX || res < INT_MIN)//int min-max kontrolü 
             exit_with_error(a);
         i++;       
     }
-    return((int) res);
+    return((int) res);//sonucu inte çeviriyoruz çünkü structta contentimizin tipini integer yaptık
 }
 
 static void parse_arg(t_list **a, const char *s)
@@ -89,21 +89,21 @@ static void parse_arg(t_list **a, const char *s)
 
     i = 0;
     start = 0;
-    if (!s || !s[0])
+    if (!s || !s[0]) //string yoksa ya da boşsa error
         exit_with_error(a);
     while(s[i])
     {
-        while(s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
+        while(s[i] && (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))) //boşluk olduğu sürece ilerle
            i++;
-        if (!s[i])
+        if (!s[i]) //boşluktan sonra string bitmişse döngüden çık
             break;
-        start = i;
+        start = i; //sayının başladığı yer 
         while(s[i] && !(s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)))
             i++;
-        val = safe_atoi_long(s, start, i - start, a);
-        if (has_dup(*a, val))
+        val = safe_atoi_long(s, start, i - start, a); // stringimizi int min-max kontrolü yapmak ve integera çevirmke için fonksiyona koyuyoruz.
+        if (has_dup(*a, val)) // stackte aynı değer daha önce varsa bu bir error durumudur 
             exit_with_error(a);
-        append_node(a, val);
+        append_node(a, val); // kontrolden geçmiş değerimizi node a koyup listenin sonuna ekliyoruz
     }
 }
 
@@ -115,7 +115,7 @@ void    build_stack(t_list **a, int argc, char **argv)
     if (!a)
         exit_with_error(a);
     while(j < argc)
-    {
+    { //argümanları sırayla ayırıyoruz
         parse_arg(a, argv[j]);
         j++;
     }
